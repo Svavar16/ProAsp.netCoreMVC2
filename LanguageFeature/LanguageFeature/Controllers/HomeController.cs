3b0c1c9b644f52ecf8ac87444614bd41b68e8d05
Svavar Pall Gudgeirsson <spg009@gmail.com>
2020-05-25 06:40:48 +0000

    adding to the HomeController and Product

--- LanguageFeature/LanguageFeature/Controllers/HomeController.cs
+++ LanguageFeature/LanguageFeature/Controllers/HomeController.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Collections.Generic;
+using LanguageFeature.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFeature.Controllers {
     public class HomeController : Controller {
         public ViewResult Index(){
-            return View(new string[] { "C#", "Language", "Features" });
+            
+            List<string> result = new List<string>();
+
+            foreach (Product p in Product.GetProducts()) {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                result.Add($"name: {name}, Price: {price}, related{relatedName}");
+            }
+            return View(result);
         }
     }
 }
--- LanguageFeature/LanguageFeature/Models/Product.cs
+++ LanguageFeature/LanguageFeature/Models/Product.cs
@@ -1,14 +1,24 @@
 namespace LanguageFeature.Models {
     public class Product {
+
+        public Product(bool stock = true) {
+            InStock = stock;
+        }
+        
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersport";
         public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; } = true;
         public static Product[] GetProducts() {
             Product kayak = new Product {
-                Name = "Kayak", Price = 275M
+                Name = "Kayak", Category = "Water Craft",Price = 275M
             };
-            Product lifejacket = new Product {
+            Product lifejacket = new Product(false) {
                 Name = "Lifejacket", Price = 48.95M
             };
+
+            kayak.Related = lifejacket;
             return new Product[] { kayak, lifejacket, null };
         }
     }
