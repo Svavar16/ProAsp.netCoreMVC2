ef9497ce51f9e1493cd2ba21cc09fb331778e026
Svavar Pall Gudgeirsson <spg009@gmail.com>
2020-05-27 10:42:05 +0000

    initializing a dictionary in the HomeController

--- LanguageFeature/LanguageFeature/Controllers/HomeController.cs
+++ LanguageFeature/LanguageFeature/Controllers/HomeController.cs
@@ -6,7 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace LanguageFeature.Controllers {
     public class HomeController : Controller {
         public ViewResult Index(){
-            return View("Index", new string[] { "Bob", "Joe", "Alice"});
+            Dictionary<string, Product> products = new Dictionary<string, Product> {
+                { "Kayak", new Product{Name = "Kayak", Price = 275M}},
+                { "Lifejacket" , new Product{ Name = "Lifejacket", Price = 48.95M}}
+            };
+            return View("Index", products.Keys);
         }
     }
 }
