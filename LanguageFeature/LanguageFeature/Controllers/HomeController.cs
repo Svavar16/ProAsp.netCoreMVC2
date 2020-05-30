5cab625f786d8bf41f834bef11e4cc607837f982
Svavar Pall Gudgeirsson <spg009@gmail.com>
2020-05-30 12:23:35 +0000

    Performing a type check in the homecontroller

--- LanguageFeature/LanguageFeature/Controllers/HomeController.cs
+++ LanguageFeature/LanguageFeature/Controllers/HomeController.cs
@@ -5,12 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFeature.Controllers {
     public class HomeController : Controller {
-        public ViewResult Index(){
-            Dictionary<string, Product> products = new Dictionary<string, Product> {
-                ["Kayak"] = new Product{Name = "Kayak", Price = 275M},
-                ["Lifejacket"] = new Product{ Name = "Lifejacket", Price = 48.95M}
-            };
-            return View("Index", products.Keys);
+        public ViewResult Index() {
+            object[] data = new object[] {275M, 29.95M, "apple", "oranges", 100, 10};
+            decimal total = 0;
+            for (int i = 0; i < data.Length; i++) {
+                if (data[i] is decimal d) {
+                    total += d;
+                }
+            }
+            return View("Index", new string[] {$"Total: {total:C2}"});
         }
     }
 }
